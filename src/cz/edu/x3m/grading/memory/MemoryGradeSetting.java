a64cd6f0fe709267fb63a670261e82649e400db5
Jan Hybš <x3mSpeedy@gmail.com>
2013-12-02 20:23:26 +0100

    Add field encapsulation

--- src/cz/edu/x3m/grading/memory/MemoryGradeSetting.java
+++ src/cz/edu/x3m/grading/memory/MemoryGradeSetting.java
@@ -8,7 +8,32 @@ import cz.edu.x3m.grading.GradingSetting;
  */
 public class MemoryGradeSetting extends GradingSetting {
 
-    public MemoryGradeSetting (Boolean isManaging) {
+    private final int limitMemoryFalling;
+    private final int limitMemoryNothing;
+
+
+
+    public MemoryGradeSetting (Boolean isManaging, int limitMemoryFalling, int limitMemoryNothing) {
         super (isManaging);
+        this.limitMemoryFalling = limitMemoryFalling;
+        this.limitMemoryNothing = limitMemoryNothing;
+    }
+
+
+
+    /**
+     * @return the limitMemoryFalling
+     */
+    public int getLimitMemoryFalling () {
+        return limitMemoryFalling;
+    }
+
+
+
+    /**
+     * @return the limitMemoryNothing
+     */
+    public int getLimitMemoryNothing () {
+        return limitMemoryNothing;
     }
 }
